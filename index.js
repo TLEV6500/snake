c50ea4fcf9e1dabf43200886a3f06906d0e3f158
Tim Villacin <timvillacin@gmail.com>
2023-10-20 20:48:41 +0800

    added increase and decrease snake length

--- index.js
+++ index.js
@@ -8,43 +8,89 @@ class GameObject {
 	constructor(objectType = "", startPos = { x: width - 1, y: height - 1 }) {
 		this.startPos = startPos;
 		this.HTMLElement = gridArr[this.startPos.y][this.startPos.x];
-		this.HTMLElement.classList.add(objectType);
+    this.HTMLElement.classList.add(objectType);
 		this.objectType = objectType;
 		// console.log(`GameObject ${this.objectType} Created at (${this.startPos.x}, ${this.startPos.y})`);
 	}
 
+  hideObject() {
+    this.HTMLElement.classList.remove(this.objectType);
+  }
+
+  showObject() {
+    if (this.HTMLElement.className.includes(this.objectType)) return;
+    this.HTMLElement.classList.add(this.objectType);
+  }
+
 	updateHTMLElement(x, y) {
-		this.HTMLElement.classList.remove(this.objectType);
+		this.hideObject();
 		this.HTMLElement = gridArr[y][x];
-		this.HTMLElement.classList.add(this.objectType);
+		this.showObject();
 	}
 }
 
 class Snake extends GameObject {
-	constructor(startPos = { x: 0, y: 0 }) {
+	constructor(startPos) {
 		super("snake", startPos);
 		this.curPos = this.startPos;
-		this.length = 1;
-		// console.log(this);
+		this.followingNode = null;
 	}
 
-	increaseLength(l = 1) {
-		this.length += l;
-		// Add more code
-	}
+  prevPos = this.startPos;
+
+  setFollowingNode(node) {
+    this.followingNode = node;
+  }
 
 	updatePos(x, y) {
 		if (x < 0 || y < 0 || x >= width || y >= height) {
 			console.log(`Snake tried going out of bounds: ${{ x, y }}`);
 			return;
 		}
+    this.prevPos = this.curPos;
 		this.curPos = { x, y };
 		super.updateHTMLElement(x, y);
+    if (this.followingNode) this.followingNode.updatePos(this.prevPos.x,this.prevPos.y);
 	}
 }
 
+class SnakeBody extends Snake {
+  constructor(nodeToFollow, nodeHead) {
+    super(nodeToFollow.prevPos);
+    this.nodeToFollow = nodeToFollow;
+    this.nodeHead = nodeHead;
+    this.nodeToFollow.setFollowingNode(this);
+  }
+  destroyThis() {
+    if (this.followingNode) return;
+    this.nodeHead.lastNode = this.nodeToFollow;
+    this.nodeToFollow.setFollowingNode = null;
+  }
+}
+
+class SnakeHead extends Snake {
+  constructor(startPos = { x: 0, y: 0 }) {
+    super(startPos);
+    this.lastNode = this;
+    this.length = 1;
+  }
+  
+	increaseLength(l = 1) {
+		this.length += l;
+    this.lastNode = new SnakeBody(this.lastNode, this);
+	}
+
+  decreaseLength(l = 1) {
+    this.length -= l;
+    // Add more code
+    this.lastNode.hideObject();
+    this.lastNode = this.lastNode.nodeToFollow
+    this.lastNode.setFollowingNode(null);
+  }
+}
+
 // Creates Snake in HTML
-const snake = new Snake();
+const snake = new SnakeHead();
 
 function initializeGrid(height, width) {
 	let cell;
@@ -67,18 +113,25 @@ gridElement.addEventListener("keydown", (event) => {
 	// console.log(event.key);
 	let curX = snake.curPos.x;
 	let curY = snake.curPos.y;
+  let newX, newY;
 	switch (event.key) {
 		case "ArrowUp":
-			snake.updatePos(curX, --curY);
+			newX = curX;
+      newY = --curY;
 			break;
 		case "ArrowRight":
-			snake.updatePos(++curX, curY);
+      newX = ++curX;
+      newY = curY;
 			break;
 		case "ArrowDown":
-			snake.updatePos(curX, ++curY);
+      newX = curX;
+      newY = ++curY;
 			break;
 		case "ArrowLeft":
-			snake.updatePos(--curX, curY);
+      newX = --curX;
+      newY = curY;
 			break;
+    default: return;
 	}
+  snake.updatePos(newX, newY);
 });
---
+++ logs.md
@@ -0,0 +1,2 @@
+### Issues
+- The second-to-the-last node becomes invisible when snake clips against itself (i.e., when you make the snake move backwards towards itself), among other bugs produced by this movement
